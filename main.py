b8a4c6366cfe6e958ddada2a12fc00bc6637b2ea
Saki <10335149+Saki-EU@users.noreply.github.com>
2019-09-12 02:34:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import yaml
 import glob
 import sys
 
-for f in glob.glob(sys.argv[1] + "**/*.json", recursive=True):
-    with open(f, "r") as fj:
-        with open(f.replace(".json", ".yml"), "w") as fy:
+if sys.argv[1].endswith(".json"):
+    with open(sys.argv[1], "r") as fj:
+        with open(sys.argv[1].replace(".json", ".yml"), "w") as fy:
             yaml.dump(json.load(fj), fy, yaml.CDumper)
-
+else:
+    for f in glob.glob(sys.argv[1] + "**/*.json", recursive=True):
+        with open(f, "r") as fj:
+            with open(f.replace(".json", ".yml"), "w") as fy:
+                yaml.dump(json.load(fj), fy, yaml.CDumper)

895b01c9b031608d997b453edf892b7e532107d7
Saki <10335149+Saki-EU@users.noreply.github.com>
2019-09-23 21:13:43 +0200

    Added arguments

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
+#!/usr/bin/env python3
 import json
 import yaml
 import glob
-import sys
+import os
+import argparse
 
-if sys.argv[1].endswith(".json"):
-    with open(sys.argv[1], "r") as fj:
-        with open(sys.argv[1].replace(".json", ".yml"), "w") as fy:
-            yaml.dump(json.load(fj), fy, Dumper=yaml.CDumper, sort_keys=False)
-else:
-    for f in glob.glob(sys.argv[1] + "**/*.json", recursive=True):
-        with open(f, "r") as fj:
-            with open(f.replace(".json", ".yml"), "w") as fy:
-                yaml.dump(json.load(fj), fy, Dumper=yaml.CDumper, sort_keys=False)
+parser = argparse.ArgumentParser()
+parser.add_argument("path", type=str, help="Path to a JSON file or directory containing JSON files.")
+parser.add_argument("-r", "--recursive", action='store_true', help="Look for files in subdirectories.")
+parser.add_argument("-s", "--sort", action='store_true', help="Sort the key-value pairs during conversion.")
+parser.add_argument("-d", "--delete", action='store_true', help="Delete file(s) after conversion.")
+args = parser.parse_args()
+
+def convert(path):
+    with open(path, "r") as fj:
+        with open(path.replace(".json", ".yml"), "w") as fy:
+            yaml.dump(json.load(fj), fy, Dumper=yaml.CDumper, sort_keys=args.sort)
+    if args.delete:
+        os.remove(path)
+    print(f"Successfully converted {path}...")
+
+
+if __name__ == "__main__":
+    if args.path.endswith(".json"):
+        convert(args.path)
+    else:
+        for path in glob.glob(args.path + "**/*.json", recursive=args.recursive):
+            convert(path)
